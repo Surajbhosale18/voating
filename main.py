24461ccd93ffe2c7e0b5fca1eb6d0c1c63d784a3
Suraj Bhosale <surajbhosale2200@gmail.com>
2023-09-26 13:18:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,4 +9,61 @@ import datetime
 import wikipedia
 # to get data from wikipedia
 import pyjokes
-# to get jokes
+# to get jokes#     engine.say(text)
+#     engine.runAndwait()
+
+recognizer =sr.Recognizer()
+with sr.Microphone() as source:
+    print("Listening...")
+    audio = recognizer.listen(source)
+
+
+try:
+    text = recognizer.recognize_google(audio)
+    print("You said:", text)
+except sr.UnknownValueError:
+    print("Sorry, I couldn't understand your speech.")
+
+
+
+
+nltk.download('punkt')
+nltk.download('stopwords')
+
+from nltk.tokenize import word_tokenize
+
+command = word_tokenize(text)
+
+
+commands = {
+    "open": "Open a file or application",
+    "search": "Perform a web search",
+    "tell me the time": "Display the current time",
+    # Add more commands and corresponding actions
+}
+
+
+if command[0] in commands:
+    action = commands[command[0]]
+    if action == "Open a file or application":
+        # Implement file/application opening logic
+        # elif action == "Perform a web search":
+        # Implement web search logic
+    elif action == "Display the current time":
+        # Implement time display logic
+    # Add more actions as needed
+else:
+    print("Command not recognized.")
+
+
+
+engine = pyttsx3.init()
+engine.say("Here is the result of your command.")
+engine.runAndWait()
+
+
+
+while True:
+    # Listen for the wake word or another trigger
+    # Execute the steps mentioned above
+
