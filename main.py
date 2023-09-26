486250336701d24ba53383625553f99479df6c37
Suraj Bhosale <surajbhosale2200@gmail.com>
2023-09-26 10:05:47 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import speech_recognition as sr 
+#  for robot to listen to our voice 
+
+import pyttsx3
+#to convert text to speech
+import pywhatkit
+#for advance controle on browser
+import datetime
+import wikipedia
+# to get data from wikipedia
+import pyjokes
+# to get jokes
